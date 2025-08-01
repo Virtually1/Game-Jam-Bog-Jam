d9dd68f7028ca8de089f7ffe9dce06f24dbcbac6
Virtual <119105087+Virtually1@users.noreply.github.com>
2025-08-01 20:59:32 +0300

    movement

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public GameObject player;
+    public float speed = 5f;
+    public float horizontalSpeed;
+    public Rigidbody2D rb;
+    public KeyCode running = KeyCode.LeftShift;
+    public float verticalSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalSpeed = Input.GetAxisRaw("Horizontal");
+        verticalSpeed = Input.GetAxisRaw("Vertical");
+        rb.velocity = new Vector2(horizontalSpeed * speed, verticalSpeed * speed);
+
+    }
+    public void Movement()
+    {
+        horizontalSpeed = Input.GetAxisRaw("Horizontal");
+        verticalSpeed = Input.GetAxisRaw("Vertical");
+        if (Input.GetKey(running))
+        {
+            speed = 10f;
+        }
+        else
+        {
+            speed = 5f; 
+        }
+            rb.velocity = new Vector2(horizontalSpeed * speed, verticalSpeed * speed);
+    }
+}
