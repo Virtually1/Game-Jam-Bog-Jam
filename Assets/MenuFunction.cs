a94eac004db6f68bfa9cda862cc6a523570495f8
Virtual <119105087+Virtually1@users.noreply.github.com>
2025-08-02 13:23:04 +0300

    Menu

--- Assets/MenuFunction.cs
+++ Assets/MenuFunction.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 public class MenuFunction : MonoBehaviour
 {
     public GameObject mainMenu;
-    public GameObject optionsMenu;
-    public GameObject exitButton;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,8 +12,25 @@ public class MenuFunction : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Menu();
+        }
+
+    }
+    public void Menu()
+    {
+        if (mainMenu.activeSelf)
+        {
+            mainMenu.SetActive(false);
+            Time.timeScale = 1f; // Resume the game
+        }
+        else
+        {
+            mainMenu.SetActive(true);
+            Time.timeScale = 0f; // Pause the game
+        }
     }
 }
