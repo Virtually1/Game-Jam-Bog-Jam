cba4e85dec9fb03c083f5026009ff06237bb9555
Virtual <119105087+Virtually1@users.noreply.github.com>
2025-08-02 22:48:36 +0300

    multiple things

---
+++ Assets/EnergyBar.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnergyBar : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Slider slider;
+    public void SetMaxEnergy(int energy)
+    {
+        slider.maxValue = energy;
+        slider.value = energy;
+    }
+    public void SetEnergy(int energy)
+    {
+        slider.value = energy;
+    }
+
+
+}
--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -10,11 +10,13 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody2D rb;
     public KeyCode running = KeyCode.LeftShift;
     public float verticalSpeed;
+    public float energy = 100f;
+    public EnergyBar energyBar;
 
     // Start is called before the first frame update
     void Start()
     {
-         
+        energyBar.SetMaxEnergy(100);
     }
 
     // Update is called once per frame
@@ -27,19 +29,20 @@ public class PlayerMovement : MonoBehaviour
     {
         horizontalSpeed = Input.GetAxisRaw("Horizontal");
         verticalSpeed = Input.GetAxisRaw("Vertical");
-        if (Input.GetKey(running))
+        if (Input.GetKey(running) && energy > 1)
         {
             speed = 10f;
+            energydrain();
         }
         else
         {
-            speed = 5f; 
+            speed = 7f;
         }
-        if(horizontalSpeed !=0)
+        if (horizontalSpeed != 0)
         {
             verticalSpeed = 0;
         }
-        if(verticalSpeed !=0)
+        if (verticalSpeed != 0)
         {
             horizontalSpeed = 0;
         }
@@ -47,6 +50,23 @@ public class PlayerMovement : MonoBehaviour
         {
             rb.velocity = new Vector2(horizontalSpeed * speed, verticalSpeed * speed);
         }
-            
+
+    }
+    public void energydrain()
+    {
+        energy -= Time.deltaTime * 3;
+        energyBar.SetEnergy((int)energy);
+
+    }
+    public void energyrecharge()
+    {
+        while (energy < 100f)
+        {
+            energy += Time.deltaTime * 7;
+        }
+        if (energy > 100f)
+        {
+            energy = 100f;
+        }
     }
 }
