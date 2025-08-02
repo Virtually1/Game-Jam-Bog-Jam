7f51cab246cb1f1268172d51ba90d1ab153afa69
Virtual <119105087+Virtually1@users.noreply.github.com>
2025-08-02 13:31:11 +0300

    Update PlayerMovement.cs

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -35,6 +35,6 @@ public class PlayerMovement : MonoBehaviour
         {
             speed = 5f; 
         }
-            rb.velocity = new Vector2(horizontalSpeed * speed, verticalSpeed * speed);
+        rb.velocity = new Vector2(horizontalSpeed * speed, verticalSpeed * speed);
     }
 }
